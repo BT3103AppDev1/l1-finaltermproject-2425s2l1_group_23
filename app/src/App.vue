3d74f8a7e2ebd45404b283927a5007d4a8e15d04
clarequek <clarequek@hotmail.com>
2025-03-10 14:37:10 +0800

    Cofigured authentication firebase

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,42 +1,20 @@
-<script setup>
-
-</script>
-
 <template>
-  <header>
-   
-  </header>
-
-  <main>
-
-  </main>
+  <div id="app">
+    <SignUp />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+import SignUp from './screens/common/SignUp.vue';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+export default {
+  name: 'App',
+  components: {
+    SignUp
   }
+};
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+<style>
+/* Add your styles here */
 </style>
---
+++ app/src/screens/common/SignUp.vue
@@ -0,0 +1,30 @@
+<template>
+    <h1>Create an account</h1>
+    <p><input type = "text" placeholder = "Email" v-model = "email"/></p>
+    <p><input type = "password" placeholder = "Password" v-model = "password"/></p>
+    <p><button @click="register">Submit</button></p>
+
+</template>
+<script setup>
+
+import { app }  from '../../../firebase/firebase.js';
+import { ref } from "vue";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const email = ref("");
+const password = ref("");
+
+const register = async () => {
+    try {
+        const auth = getAuth(app);
+        const userCredential = await createUserWithEmailAndPassword(auth, email.value, password.value);
+        console.log('Successfully registered', userCredential);
+    } catch (error) {
+        console.error('Error registering', error);
+    }
+};
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
