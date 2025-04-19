772465073dfadddbe74ffd0205c0ecd7f3fa2916
zzhengweii <ow.zhengwei@gmail.com>
2025-04-19 22:55:22 +0800

    ediited vite.config.js

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,12 +1,18 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
+import nodePolyfills from "node-polyfill";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    nodePolyfills({
+      crypto: true, // Enable the polyfill for the crypto module
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
